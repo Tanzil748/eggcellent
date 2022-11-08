e20e932b30a1a4477ac9edd8b8d7613335e879f8
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-11-07 22:10:57 -0500

    Footer & navigation fixed

--- egg_website/src/App.jsx
+++ egg_website/src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Content from "./components/Content";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./pages";
+import About from "./pages/about";
+import Menu from "./pages/menu";
+import Contact from "./pages/contact";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Hero />
-      <Content />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="menu" element={<Menu />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
--- egg_website/src/components/Footer.jsx
+++ egg_website/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <section className="flex justify-center items-center h-16 bg-black text-white">
+      <p>CopyRight © EGGY FOOTER</p>
+    </section>
+  );
 };
 
 export default Footer;
