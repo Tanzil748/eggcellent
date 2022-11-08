f9d19845da0e4e4793e0278d863d6c649862c435
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-11-07 22:29:21 -0500

    Dropdown done

--- egg_website/src/App.jsx
+++ egg_website/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
@@ -6,11 +6,32 @@ import Home from "./pages";
 import About from "./pages/about";
 import Menu from "./pages/menu";
 import Contact from "./pages/contact";
+import Dropdown from "./components/Dropdown";
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log("Menu Resized");
+      }
+    };
+    window.addEventListener("resize", hideMenu);
+
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
--- egg_website/src/components/Navbar.jsx
+++ egg_website/src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { AiOutlineMenu } from "react-icons/ai";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <section className="flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono">
       <Link to="/" className="pl-8">
         Egg
       </Link>
-      <div className="px-4 cursor-pointer md:hidden">
+      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
         <AiOutlineMenu />
       </div>
       <div className="pr-8 md:block hidden">
