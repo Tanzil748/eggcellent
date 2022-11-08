736a190de07ea7108e78c5387baa5122017cd345
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-11-07 19:31:15 -0500

    Hero done

--- egg_website/src/components/Hero.jsx
+++ egg_website/src/components/Hero.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="bg-white h-screen flex flex-col justify-center items-center">
+      <h1 className="lg:text-9xl md:text-7xl sm:text-5xl text-3xl font-black mb-14">
+        EGGCELLENT
+      </h1>
+      <Link className="py-6 px-10 bg-yellow-600 rounded-full text-3xl hover:bg-yellow-500 transition duration-500 ease-in-out flex items-center animate-bounce">
+        Order Now <AiOutlineShoppingCart className="ml-4" />
+      </Link>
+    </section>
+  );
 };
 
 export default Hero;
